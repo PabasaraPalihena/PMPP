9793df467918212e00a18f1c44c1930b4c0a9947
PabasaraPalihena <pabapalihena@gmail.com>
2026-05-09 11:46:27 +0100

    added github configuration

--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
    devIndicators: false,
+   output: "export",
+   basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
 };
 
 export default nextConfig;
